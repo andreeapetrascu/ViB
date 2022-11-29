79125f2b4a18e2e1d9cb96778dc5eabde754d8cc
andreeapetrascu <andreea.petrascu.e@gmail.com>
2022-11-29 12:47:48 +0200

    geolocator prep

--- frontend/lib/screens/hellopage/hellopage.dart
+++ frontend/lib/screens/hellopage/hellopage.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../home/components/home_screen.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HelloPage extends StatelessWidget {
   Widget build(BuildContext context) {
